c8f3de1d53aac9ecababde2e9b23519fd9cb807f
Naveen <119277869+Navin2404@users.noreply.github.com>
2026-05-30 09:53:58 +0530

    app js base url changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
   Loader2
 } from 'lucide-react';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_URL = "https://ai-resume-analyser.onrender.com"
 
 const SUGGESTED_QUESTIONS = [
   "What are the technical skills?",
