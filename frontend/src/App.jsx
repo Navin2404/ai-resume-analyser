487af6b05e7b61f2f4f95e408c1c1aeb528127ab
Naveen <119277869+Navin2404@users.noreply.github.com>
2026-05-30 12:07:10 +0530

    url changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
   Loader2
 } from 'lucide-react';
 
-const API_URL  = "https://ai-resume-analyser-l5a8.onrender.com/";
+const API_URL  = "https://ai-resume-analyser-l5a8.onrender.com";
 
 const SUGGESTED_QUESTIONS = [
   "What are the technical skills?",
